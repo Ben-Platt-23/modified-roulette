5c4eaf5ee1e641ecbc36bfe8c2f222c54e7d6a01
Ben Platt <73078368+monkboyb@users.noreply.github.com>
2021-12-24 20:34:21 -0600

    changes

--- Modified_Roulette_Final.py
+++ Modified_Roulette_Final.py
@@ -5,15 +5,82 @@ import math
 import random
 
 # Creates a list called 'cards' that stores every card 
-global cards 
 cards = ['2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5', '6', '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', 'Jack', 'Jack', 'Jack', 'Jack', 'Queen', 'Queen', 'Queen', 'King', 'King', 'Ace']
 # Creates a list called 'card_ids' that stores one of every card to be used in the draw_a_card function to check the entry
-global card_ids
 card_ids = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+global roulette_spin 
+roulette_spin = ''
+global player_result
+player_result = ''
+global play_again
+play_again = False
+global player_bet
+player_bet = 0
+global bet_is_ready
+bet_is_ready = False
+
+
+
+# Creates the system for payout
+def betting_system():
+    global betting_weight
+    betting_weight = 1
+    global player_payout
+    player_payout = 0
+    global card_weight
+    card_weight = 0
+
+
+
+    if roulette_spin == 'Ace':
+        card_weight = 13
+    elif roulette_spin == 'King':
+        card_weight = 12
+    elif roulette_spin == 'Queen':
+        card_weight = 11
+    elif roulette_spin == 'Jack':
+        card_weight = 10
+    elif roulette_spin == '10':
+        card_weight = 9
+    elif roulette_spin == '9':
+        card_weight = 8
+    elif roulette_spin == '8':
+        card_weight = 7
+    elif roulette_spin == '7':
+        card_weight = 6
+    elif roulette_spin == '6':
+        card_weight = 5
+    elif roulette_spin == '5':
+        card_weight = 4
+    elif roulette_spin == '4':
+        card_weight = 3
+    elif roulette_spin == '3':
+        card_weight = 2
+    elif roulette_spin == '2':
+        card_weight = 1
+
+
+    if player_bet >= 10000 and player_bet < 20000:
+        betting_weight += 2
+        player_payout = (card_weight + betting_weight) * player_bet
+    elif player_bet >= 20000 and player_bet < 30000:
+        betting_weight += 3
+        player_payout = (card_weight + betting_weight) * player_bet
+    elif player_bet >= 30000 and player_bet < 40000:
+        betting_weight += 4
+        player_payout = (card_weight + betting_weight) * player_bet
+    elif player_bet >= 40000 and player_bet < 50000:
+        betting_weight += 5
+        player_payout = (card_weight + betting_weight) * player_bet
+    elif betting_weight == 1:
+        player_payout = (card_weight + betting_weight) * player_bet
+
+
 
 
 # Creates the function for the betting system
-def betting_system():
+def bet_intake_system():
+    betting_system()
     # Creates a global variable for the amount of cards 
     global amount_of_cards 
     # Sets amount of cards equal to 0 
@@ -21,21 +88,32 @@ def betting_system():
     # Creates a global variable for whether the player is betting or not (are they playing or not)
     global is_player_betting 
     # Sets the is player betting variable to False 
-    is_player_betting = False
-    # Creats a global variable for if the bet is ready
-    global bet_is_ready
-    bet_is_ready = False
+    is_player_betting = False    
+    # Creates a global variable for the players bet 
+    global player_bet
+    global is_bet_ready
+    global player_balance
+    player_balance = 0
+    player_balance = player_balance + player_payout
+
+    
+    
+            
+
 
     # Creates a while loop to keep going until the is player betting variable does not equal False
     while is_player_betting == False:
-        # Starts the game by printing a welcome message
-        print('Welcome to modified roulette.')
-        # Write a print statement explaining the game
-        print('If you bet $10,000 you get to pick one card. Every extra $10,000 you bet, you get an extra card to draw. (Limit of 5 cards per player)')
-        # Creates a global variable for the players bet 
-        global player_bet
-        # Sets the players bet variable to their input and makes it an integer no matter what
-        player_bet = int(input('Please enter your bet.'))
+        start_game = input('Welcome to modified roulette. If you are ready to play enter "ready". (CASE SENSITIVE)')
+        while start_game != 'ready':
+            start_game = input('Welcome to modified roulette. If you are ready to play enter "ready". (CASE SENSITIVE)')
+        if start_game == 'ready':
+            print('Balance: $' + str(player_balance))
+            start_bet = input('If you are ready to bet, enter "ready". (CASE SENSITIVE)')
+        while start_bet != 'ready':
+            start_bet = input('If you are ready to bet, enter "ready". (CASE SENSITIVE)')
+        if start_bet == 'ready':
+            print('If you bet $10,000 you get to pick one card. Every extra $10,000 you bet, you get an extra card to draw. (Limit of 5 cards per player)')
+            player_bet = int(input('Please enter your bet.'))
 
         #
         if player_bet >= 10000 and player_bet <= 20000:
@@ -103,10 +181,26 @@ def draw_a_card(cards):
     print(random_cards)
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Creates a function that will call the betting system function then the draw a card function and encapsulate the calculations for the games result and will print them out
 def modified_roulette():
-    # Calls the betting system function to start the game
-    betting_system()
+
+    bet_intake_system()
+    
 
     # Checks if the bet is ready
     if bet_is_ready == True:
@@ -151,32 +245,39 @@ def modified_roulette():
             # While ready to spin does NOT equal 'ready' then continue to ask the player if they are ready to spin 
             ready_to_spin = input('If you are ready to spin enter "ready". (CASE SENSITIVE')
 
-    # Checks if the player only chose one card
-    if amount_of_cards == 1:
-        # If the player only chose to draw one card then the game checks if the players card matched the winning card 
-        if roulette_spin == random_cards:
-            # If one of the players cards matches the winning card then it prints a congratulations statement 
-                print('Congratulations, you won!!!')
-        # Else
-        else:
-            # If 'Else' (the player lost or didnt win) then print out the sorry message
-            print('Sorry, you lose.')
-    # Checks if the winnning card is equal to any of the players cards if the player chose more then one card to draw
-    if amount_of_cards > 1:
-        for roulette_spin in random_cards:
-            if roulette_spin in random_cards:
-                # If one of the players cards matches the winning card then it prints a congratulations statement 
-                print('Congratulations, you won!!!')
-            # Else
-            else:
-                # If 'Else' (the player lost or didnt win) then print out the sorry message
-                print('Sorry, you lose.')
+
     
 
     for card in roulette_spin:
         if card in random_cards:
+            betting_system()
+            player_result = 'won'
+            print('Congratulations, you won!!!')
+            print('Payout: $' + str(player_payout))
+            play_again_input = input('If you would like to play again please enter "play". (CASE SENSITIVE)')
+            
+            while play_again_input != 'play':
+                play_again_input = input('If you would like to play again please enter "play". (CASE SENSITIVE)')
+            if play_again_input == 'play':
+                modified_roulette()
+            
+            
+     
+        else:
+            player_result = 'lost'
+            print('Sorry, you lose.')
+            print('You lost everything you bet.')
+            play_again_input = input('If you would like to play again please enter "play". (CASE SENSITIVE)')
+            
+            while play_again_input != 'play':
+                play_again_input = input('If you would like to play again please enter "play". (CASE SENSITIVE)')
+            if play_again_input == 'play':
+                modified_roulette()
+           
             
 
 
 
 modified_roulette()
+
+
---
+++ test_script.py
@@ -0,0 +1,10 @@
+roulette_spin = 'Ace'
+
+card_weights = {
+    "Ace": 13
+}
+
+if roulette_spin == card_weights["Ace"]:
+    print("Its Lit")
+else:
+    print("It aint work")
